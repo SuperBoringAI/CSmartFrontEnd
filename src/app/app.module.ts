ef82cba79f43a7ace5b1bebe642021959935ecba
SuperBoringAI <x.deerme@gmail.com>
2022-09-27 02:29:59 +0300

    NS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -8,8 +10,6 @@ import { MatTableModule } from '@angular/material/table';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogoComponent } from './component/logo/logo.component';
 import { HeaderComponent } from './component/header/header.component';
@@ -19,7 +19,6 @@ import { EmployeeComponent } from './component/employee/employee.component';
 import { EmployeeNewComponent } from './component/employee-new/employee-new.component';
 import { EmployeeUpdateComponent } from './component/employee-update/employee-update.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
